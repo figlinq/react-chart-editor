28f3a607d0e5d17883c4dcfbaf9dc31a1678e3ec
dmt0 <dmitry@plot.ly>
2023-02-23 21:55:15 -0500

    Refactor

--- src/components/containers/PanelEmpty.js
+++ src/components/containers/PanelEmpty.js
@@ -2,21 +2,17 @@ import PropTypes from 'prop-types';
 import {ChartLineIcon} from '@figlinq/plotly-icons';
 import {bem} from 'lib';
 
-export const PanelMessage = (props) => {
-  const {children, icon: Icon} = props;
-  const heading = props.heading || '';
-  return (
-    <div className="panel__empty__message">
-      {Boolean(Icon) && (
-        <div className="panel__empty__message__icon">
-          <Icon />
-        </div>
-      )}
-      {Boolean(heading) && <div className="panel__empty__message__heading">{heading}</div>}
-      <div className="panel__empty__message__content">{children}</div>
-    </div>
-  );
-};
+export const PanelMessage = ({children, heading, icon: Icon}) => (
+  <div className="panel__empty__message">
+    {Boolean(Icon) && (
+      <div className="panel__empty__message__icon">
+        <Icon />
+      </div>
+    )}
+    {Boolean(heading) && <div className="panel__empty__message__heading">{heading}</div>}
+    <div className="panel__empty__message__content">{children}</div>
+  </div>
+);
 
 PanelMessage.defaultProps = {
   icon: ChartLineIcon,
